7bb49cd77fc9b32e9d890041d459b98798c3af2e
chomsky <leonhardat@gmail.com>
2018-09-15 22:44:37 +0200

    Changed text

--- steganography.js
+++ steganography.js
@@ -22,5 +22,17 @@ function shift(image){
 }
 
 function combine(show,hide){
-    var answer =
+    var answer = new SimpleImage(show.getWidth(),show.getHeight());
+    for (var px of answer.values()){
+        var x = px.getX();
+        var y = px.getY();
+        var showPixel = show.getPixel(x,y);
+        var hidePixel = hide.getPixel(x,y);
+        px.setRed(showPixel.getRed() + hidePixel.getRed());
+        px.setGreen(showPixel.getGreen() + hidePixel.getGreen());
+        px.setBlue(showPixel.getBlue() + hidePixel.getBlue());
+    }
+    return answer;
 }
+
+
